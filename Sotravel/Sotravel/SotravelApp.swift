0af4676164edc01b3026373b267428b9e491cd79
rizhaow <rizhaow@gmail.com>
2023-03-09 12:47:53 +0800

    feat: add login view

---
+++ Sotravel/Sotravel/SotravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SotravelApp.swift
+//  Sotravel
+//
+//  Created by Apple on 9/3/23.
+//
+
+import SwiftUI
+
+@main
+struct SotravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
