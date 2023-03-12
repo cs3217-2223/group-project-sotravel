d656ca5d79dc0c85c1085513e205640b081e5386
rizhaow <rizhaow@gmail.com>
2023-03-12 15:33:05 +0800

    fix: env object

--- Sotravel/Sotravel/SotravelApp.swift
+++ Sotravel/Sotravel/SotravelApp.swift
@@ -16,6 +16,7 @@ struct SotravelApp: App {
         WindowGroup {
             ContentView()
                 .environmentObject(user)
+                .environmentObject(events)
         }
     }
 }
