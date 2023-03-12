1d2da2702e1954e363c1f161b63ea1b6c9a223b1
wweqg <zhangwweqg@gmail.com>
2023-03-12 16:40:55 +0800

    feat: friend profile view

--- Sotravel/Sotravel/Views/ProfilePageView/ProfileHeaderView.swift
+++ Sotravel/Sotravel/Views/ProfilePageView/ProfileHeaderView.swift
@@ -31,38 +31,7 @@ struct ProfileHeaderView: View {
                 .foregroundColor(.gray)
                 .multilineTextAlignment(.center)
 
-            HStack(spacing: 20) {
-                if let telegramUsername = user.telegramUsername, !telegramUsername.isEmpty {
-                    Button(action: {
-                        guard let url = URL(string: "https://t.me/\(telegramUsername)") else { return }
-                        UIApplication.shared.open(url)
-                    }, label: {
-                        Image("telegram")
-                            .resizable()
-                            .frame(width: 30, height: 30)
-                    })
-                }
-                if let instagramUsername = user.instagramUsername, !instagramUsername.isEmpty {
-                    Button(action: {
-                        guard let url = URL(string: "https://www.instagram.com/\(instagramUsername)/") else { return }
-                        UIApplication.shared.open(url)
-                    }, label: {
-                        Image("instagram")
-                            .resizable()
-                            .frame(width: 30, height: 30)
-                    })
-                }
-                if let tiktokUsername = user.tiktokUsername, !tiktokUsername.isEmpty {
-                    Button(action: {
-                        guard let url = URL(string: "https://www.tiktok.com/@\(tiktokUsername)") else { return }
-                        UIApplication.shared.open(url)
-                    }, label: {
-                        Image("tiktok")
-                            .resizable()
-                            .frame(width: 30, height: 30)
-                    })
-                }
-            }.padding(.vertical, 12)
+            SocialMediaLinksView(user: user)
         }
     }
 }
