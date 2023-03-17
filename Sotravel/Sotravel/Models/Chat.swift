86ae87e9e48e5a4b107706edc910199dfde060fb
Neo Wei Qing <ztolenstar@hotmail.com>
2023-03-17 17:27:19 +0800

    update mock

--- Sotravel/Sotravel/Models/Chat.swift
+++ Sotravel/Sotravel/Models/Chat.swift
@@ -1,6 +1,10 @@
 class Chat {
     var messages = [ChatMessage]()
 
+    init(messages: [ChatMessage] = [ChatMessage]()) {
+        self.messages = messages
+    }
+
     func addChatMessage(_ message: ChatMessage) {
         messages.append(message)
     }
