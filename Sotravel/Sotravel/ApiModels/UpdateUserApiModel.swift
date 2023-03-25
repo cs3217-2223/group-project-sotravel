a43f6f7ee6e085ea3cae544197e327b7552cd86d
Azeem Vasanwala <azeem.iron@gmail.com>
2023-03-25 13:41:35 +0800

    Update UpdateUserApiModel to include last_name field

--- Sotravel/Sotravel/ApiModels/UpdateUserApiModel.swift
+++ Sotravel/Sotravel/ApiModels/UpdateUserApiModel.swift
@@ -8,19 +8,21 @@
 import Foundation
 
 struct UpdateUserApiModel: Encodable {
-    let socials_tiktok: String?
-    let socials_instagram: String?
-    let socials_telegram: String?
     let first_name: String?
+    let last_name: String?
     let email: String?
     let image: String?
     let description: String?
+    let socials_tiktok: String?
+    let socials_instagram: String?
+    let socials_telegram: String?
 
     init(user: User) {
         self.socials_tiktok = user.tiktokUsername
         self.socials_instagram = user.instagramUsername
         self.socials_telegram = user.telegramUsername
-        self.first_name = user.name
+        self.first_name = user.firstName
+        self.last_name = user.lastName
         self.email = user.email
         self.image = user.imageURL
         self.description = user.desc
