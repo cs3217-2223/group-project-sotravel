c2fa0a8bc41e82a00fbbff754e38b6d244076c7f
Zhang Weiqiang <zhangwweqg@gmail.com>
2023-03-30 23:43:55 +0800

    update friendService and getEvent method

---
+++ Sotravel/Sotravel/Service/FriendService.swift
@@ -0,0 +1,24 @@
+//
+//  FriendService.swift
+//  Sotravel
+//
+//  Created by Weiqiang Zhang on 30/3/23.
+//
+
+import Foundation
+import Resolver
+
+class FriendService: ObservableObject {
+
+    private var friendsCache: [UUID: User]
+
+    @Injected private var userRepository: UserRepository
+
+    init() {
+        self.friendsCache = [:]
+    }
+
+    func getFriend(id: UUID) throws -> User {
+        mockUser1
+    }
+}
